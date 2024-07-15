1be081b01332a7ede0d48013583b0319a497ce9a
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-15 12:34:27 +0530

    [Rajesh] Add : Password validation (min 8 character)

--- main.py
+++ main.py
@@ -2,31 +2,56 @@
 # As a User need to enter a valid Last Name - Last name starts with Cap and has minimum 3 characters
 # As a User need to enter a valid email - E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
 # As a User need to follow pre -defined Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
+# As a User need to follow pre-defined Password rules. 
+# Rule 1 – minimum 8 Characters
+
 
 import re
 
-def validate_string(user_input, regex):
-    if re.search(regex, user_input):
+Constrains = [
+    {
+        'title': 'First Name',
+        'regex': '^[A-Z][a-z]{2,}$',
+        'message': 'First name starts with Cap and has minimum 3 characters: '
+    },
+    {
+        'title': 'Last Name',
+        'regex': '^[A-Z][a-z]{2,}$',
+        'message': 'Last name starts with Cap and has minimum 3 characters: '
+    },
+    {
+        'title': 'Email',
+        'regex': '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',
+        'message': 'Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions: '
+    },
+    {
+        'title': 'Phone',
+        'regex': '^91 [0-9]{10}$',
+        'message': 'Country code follow by space and 10 digit number: '
+    },
+    {
+        'title': 'Password',
+        'regex': '^.{8,}$',
+        'message': 'Password must be minimum 8 characters: '
+    }
+]
+
+def check_validity(regex, value):
+    if re.search(regex, value):
         return True
     else:
         return False
 
-name_regex = '^[A-Z][a-z]{2,}$'
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
+def validate():
+    data = {}
+    for constrain in Constrains:
+        value = input(constrain['message'])
+        while not check_validity(constrain['regex'], value):
+            print(f'Invalid input for {constrain["title"]}. Please try again.')
+            value = input(constrain['message'])
+        data[constrain['title']] = value
+        print(f'Valid {constrain["title"]}: {value}')
+    return data
 
-email_regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-email = input("Enter your email: ")
-
-phone_regex = '^[0-9]{2} [0-9]{10}$'
-phone = input("Enter your phone number: ")
-
-if validate_string(phone, phone_regex):
-    print("Valid input")
-else:
-    print("Invalid input")
-
-if validate_string(first_name, name_regex) and validate_string(last_name,name_regex) and validate_string(email, email_regex):
-    print("Valid input")
-else:
-    print("Invalid input")
+data = validate()
+print(data)
