8925d0e2777b9eb5930596bda728bef75780fb4c
DevRajeshSingh <rajeshnksingh@gmail.com>
2024-07-15 11:26:50 +0530

    [Rajesh] Add : User Last Name Input Pattern

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # As a User need to enter a valid First Name - First name starts with Cap and has minimum 3 characters
+# As a User need to enter a valid Last Name - Last name starts with Cap and has minimum 3 characters
 
 import re
 
-def validate_first_name(first_name):
-    regex = '^[A-Z][a-z]{2,}$'
-    if re.search(regex, first_name):
+def validate_string(user_input, regex):
+    if re.search(regex, user_input):
         return True
     else:
         return False
 
-
+regex = '^[A-Z][a-z]{2,}$'
 first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
-if(validate_first_name(first_name)):
-    print("Valid first name")
+if validate_string(first_name, regex) and validate_string(last_name, regex):
+    print("Valid input")
 else:
-    print("Invalid first name")
+    print("Invalid input")
